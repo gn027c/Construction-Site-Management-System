2c7e74875085214ef53f2c58b6fa156f4ff72b15
Trần Ngọc Anh Tuấn <tuantran01224394077@gmail.com>
2026-09-18 13:30:46 +0700

    Update Worker.java

--- src/model/Worker.java
+++ src/model/Worker.java
@@ -8,43 +8,43 @@ package model;
  * - Chịu trách nhiệm quản lý thông tin ngành nghề, chứng chỉ an toàn và tổ đội thi công.
  * - Cài đặt các hành vi hiển thị chi tiết và chuẩn bị dữ liệu xuất báo cáo theo yêu cầu Workshop 1.
  */
-public class Worker extends Person {
-      private String maNguoi;
-    private String hoTen;
-    private String ngaySinh;
-    private String gioiTinh;
-    private String soDienThoai;
- 
-    public Person() {
-      
+public class Worker {
+      private String personId;
+      private String fullName;
+      private String dateOfBirth;
+      private String gender;
+      private String phoneNumber;
+
+    public Worker() {
+
     }
- 
-    public Person(String maNguoi, String hoTen, String ngaySinh, String gioiTinh, String soDienThoai) {
-        this.maNguoi = maNguoi;
-        this.hoTen = hoTen;
-        this.ngaySinh = ngaySinh;
-        this.gioiTinh = gioiTinh;
-        this.soDienThoai = soDienThoai;
+
+    public Worker(String personId, String fullName, String dateOfBirth, String gender, String phoneNumber) {
+        this.personId = personId;
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
     }
- 
-    public String getMaNguoi() { return maNguoi; }
-    public void setMaNguoi(String maNguoi) { this.maNguoi = maNguoi; }
- 
-    public String getHoTen() { return hoTen; }
-    public void setHoTen(String hoTen) { this.hoTen = hoTen; }
- 
-    public String getNgaySinh() { return ngaySinh; }
-    public void setNgaySinh(String ngaySinh) { this.ngaySinh = ngaySinh; }
- 
-    public String getGioiTinh() { return gioiTinh; }
-    public void setGioiTinh(String gioiTinh) { this.gioiTinh = gioiTinh; }
- 
-    public String getSoDienThoai() { return soDienThoai; }
-    public void setSoDienThoai(String soDienThoai) { this.soDienThoai = soDienThoai; }
- 
-    
-    public abstract void hienThiThongTin();
- 
-  
-    public abstract List<String> chuanBiDuLieuXuatBaoCao();
+
+    public String getPersonId() { return personId; }
+    public void setPersonId(String personId) { this.personId = personId; }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getDateOfBirth() { return dateOfBirth; }
+    public void setDateOfBirth(String dateOfBirth) { this.dateOfBirth = dateOfBirth; }
+
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+
+    public abstract void displayInfo();
+
+
+    public abstract List<String> prepareReportData();
 }
